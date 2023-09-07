8274f05b2b73a685be3f2a86af7bd1ddcdf98611
MUSAVIR REHMAN <musavirrehman4878@gmail.com>
2023-09-07 14:27:01 +0530

    added triple equals

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -23,7 +23,7 @@ const SignUpForm = () => {
     const handleSubmit = async (event) => {
         event.preventDefault()
         
-        if(password != confirmPassword) {
+        if(password !== confirmPassword) {
             alert("password do not match")
             return
         }
@@ -34,7 +34,7 @@ const SignUpForm = () => {
             resetFormFields()
 
         } catch(error){
-            if(error.code == 'auth/email-already-in-use') {
+            if(error.code === 'auth/email-already-in-use') {
                 alert("Cannot create user,email already in use")
             } else{
             console.log('user creation encountered an error',error);
